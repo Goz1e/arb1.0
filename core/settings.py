dcff7c811e4d20d5209bf70a0b07f901ac57b7a1
gozz <phenominal55@outlook.com>
2023-05-03 09:54:03 +0100

    aws_default_acl

--- core/settings.py
+++ core/settings.py
@@ -227,3 +227,4 @@ AWS_LOCATION = 'static'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_DEFAULT_ACL = None
